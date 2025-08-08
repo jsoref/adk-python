3bd79257c0d5b07a314910f230dcaa86e8dc4914
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-08 16:31:35 -0400

    chore: Fix spelling - unstructured
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- contributing/samples/adk_answering_agent/upload_docs_to_vertex_ai_search.py
+++ contributing/samples/adk_answering_agent/upload_docs_to_vertex_ai_search.py
@@ -135,7 +135,7 @@ def import_from_gcs_to_vertex_ai(
         # parent has the format of
         # "projects/{project_number}/locations/{location}/collections/{collection}/dataStores/{datastore_id}/branches/default_branch"
         parent=full_datastore_id + "/branches/default_branch",
-        # Specify the GCS source and use "content" for unstructed data.
+        # Specify the GCS source and use "content" for unstructured data.
         gcs_source=discoveryengine.GcsSource(
             input_uris=[gcs_uri], data_schema="content"
         ),
